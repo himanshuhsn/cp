054afcf3d0319d18cd7bb1d3a3a1f4390794b7c2
Himanshu <49199845+himanshuhsn@users.noreply.github.com>
2023-01-15 20:23:52 +0530

    updated from cp algorithm

--- cosa_raju.cpp
+++ cosa_raju.cpp
@@ -1,89 +1,61 @@
 /**
  * Kosa Raju algo is used to find the strongly connected component.
- * Returns a vector of vector contains Strongly connected components in each vector.
+ * Provides the components for each iteration.
  */
 
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
 
-vector<vector<int>> g;
-vector<vector<int>> inv_g;
+vector<vector<int>> adj, adj_rev;
+vector<bool> used;
+vector<int> order, component;
 
-vector<int> p;
-vector<int> out_mark;
-void out_dfs(int v) {
-    out_mark[v] = 1;
-    for(auto u: g[v]) {
-        if(out_mark[u] == 0) {
-            out_dfs(u);
-        }
-    }
-    p.push_back(v);
+void dfs1(int v) {
+    used[v] = true;
+
+    for (auto u : adj[v])
+        if (!used[u])
+            dfs1(u);
+
+    order.push_back(v);
 }
 
-vector<int> in_mark;
-void in_dfs(int v, vector<int>& cc) {
-    in_mark[v] = 1;
-    for(auto u: inv_g[v]) {
-        if(in_mark[u] == 0) {
-            in_dfs(u, cc);
-        }
-    }
-    cc.push_back(v);
+void dfs2(int v) {
+    used[v] = true;
+    component.push_back(v);
+
+    for (auto u : adj_rev[v])
+        if (!used[u])
+            dfs2(u);
 }
 
-vector<vector<int>> kosa_raju() {
-    int N = g.size();
-    vector<vector<int>> ans;
-    out_mark.resize(N, 0);
-    for(int i=0; i<N; i++) {
-        if(out_mark[i] == 0) {
-            out_dfs(i);
+int main() {
+    int n;
+    // ... read n ...
+
+    for (;;) {
+        int a, b;
+        // ... read next directed edge (a,b) ...
+        adj[a].push_back(b);
+        adj_rev[b].push_back(a);
+    }
+
+    used.assign(n, false);
+
+    for (int i = 0; i < n; i++)
+        if (!used[i])
+            dfs1(i);
+
+    used.assign(n, false);
+    reverse(order.begin(), order.end());
+
+    for (auto v : order)
+        if (!used[v]) {
+            dfs2 (v);
+
+            // ... processing next component ...
+
+            component.clear();
         }
-    }
-    reverse(p.begin(), p.end());
-
-    in_mark.resize(N, 0);
-    for(auto it: p) {
-        if(in_mark[it] == 0) {
-            vector<int> vt;
-            in_dfs(it, vt);
-            ans.push_back(vt);
-        }
-    }
-
-    return ans;
-}
-
-void solve(){
-    int n, m; cin >> n >> m;
-    g.resize(n);
-    inv_g.resize(n);
-    for(int i=0; i<m; i++) {
-        int v, u; cin >> v >> u;
-        v--; u--;
-        g[v].push_back(u);
-        inv_g[u].push_back(v);
-    }
-
-    vector<vector<int>> scc = kosa_raju();
-    for(auto cc: scc) {
-        for(auto v: cc) {
-            cout << v << " ";
-        }
-        cout << "\n";
-    }
-}
-
-int32_t main(){
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-	int test_cases = 1;
-	//cin >> test_cases;
-	while(test_cases--){
-		solve();
-	}
-	return 0;
 }
